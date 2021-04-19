a10f62d20469178fee9bc1a1efeadfc4ae18c289
castle.jaemin <ehdrud1332@gmail.com>
2021-04-19 23:44:07 +0900

    2 TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 // AppModule은 main.ts로 import되는 유일한 모듈이다.
@@ -11,11 +11,23 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'seongjaemin',
+      password: '1225',
+      database: 'nuber-eats',
+      // synchronize: true는 TypeORM이 데이터베이스에 연결할 때 데이터베이스를 나의 모듈의 현재 상태로 마이스래이션한다는 뜻
+      synchronize: true,
+      // logging은 데이터베이스에서 무슨 일이 일어나는지 콘솔에 표시하는것
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       // 기본적으로 이것이 schema파일을 만들어낸다. schema.gql파일을 따로 가지고 있지 않아도 된다는 의미.
       autoSchemaFile: true,
     }),
-    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,7 +1,6 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
-import { create } from 'domain';
 
 @Resolver(of => Restaurant) // 우리의 resolver는 Restaurant의 resolver가 됐다. 굳이 이렇게 쓰지 않아도 된다.
 export class RestaurantsResolver {
@@ -14,10 +13,8 @@ export class RestaurantsResolver {
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     return [];
   }
-  // inputType은 기본적으로 object 통째로 전달할 수 있도록 해준다.
   @Mutation(returns => Boolean)
   createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
-    console.log(createRestaurantDto);
     return true;
   }
 }
