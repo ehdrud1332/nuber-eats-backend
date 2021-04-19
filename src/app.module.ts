273161ef0923b318d7148c7cf3475e3af85b7f7b
castle.jaemin <ehdrud1332@gmail.com>
2021-04-20 00:14:56 +0900

    2.2 Configuring ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,14 +16,19 @@ import { ConfigModule } from '@nestjs/config';
       // IsGlobal은 나의 어플리케이션의 어디서나 config 모듈에 접근할 수 있다는 것
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      // ignoreEnvFile은 서버에 deploy 할 때 환경변수 파일을 사용하지 않는다는것.
+      // production 환경일 때는 ConfigModule이 환경변수 파일을 무시한다.
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      // validationSchema라는 건 내가 원하는 모든 환경 변수의 유효성을 검사할 수 있다.
     }),
     TypeOrmModule.forRoot({
+      // TypeORM의 설정옵션은 port가 number여야한다.
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'seongjaemin',
-      password: '1225',
-      database: 'nuber-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       // synchronize: true는 TypeORM이 데이터베이스에 연결할 때 데이터베이스를 나의 모듈의 현재 상태로 마이스래이션한다는 뜻
       synchronize: true,
       // logging은 데이터베이스에서 무슨 일이 일어나는지 콘솔에 표시하는것
