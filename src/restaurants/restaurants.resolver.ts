f11c2e2414a234e61f4fdd5eb7fce45b2072510b
castle.jaemin <ehdrud1332@gmail.com>
2021-04-19 16:40:30 +0900

    1.2 Arguments

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(of => Restaurant) // 우리의 resolver는 Restaurant의 resolver가 됐다. 굳이 이렇게 쓰지 않아도 된다.
@@ -7,8 +7,9 @@ export class RestaurantsResolver {
   // Query가 return하고자 하는 type을 return하는 function이여야 한다는것이다.
   // Query는 첫번째 args로 함수가 필요하다.
 
-  @Query(returns => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query(returns => [Restaurant]) // gql에서는 [Restaurant] 이렇게 표현
+  // ts에서는 Restaurant[] 이렇게 표현
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
   }
 }
