371552490829206aaf2443a1e7d2bcffa56a7393
castle.jaemin <ehdrud1332@gmail.com>
2021-04-19 16:30:37 +0900

    1.1 ObjectType
    ObjectType은 database의 model 같은 역할을 한다.

---
+++ src/restaurants/entities/restaurant.entity.ts
@@ -0,0 +1,11 @@
+// entity는 우리의 데이터베이스에 있는 모델을 상상하면 된다.
+import { Field, ObjectType } from '@nestjs/graphql';
+
+//ObjectType을 만들어줌으로써 이제 Restaurant Entity를 가지게 됐다.
+@ObjectType()
+export class Restaurant {
+  @Field(type => String) // 이 부분은 graphQl을 위한 return
+  name: string; // 이건 ts를 위한 return
+  @Field(type => Boolean, { nullable: true })
+  isGood?: boolean;
+}
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,14 @@
 import { Resolver, Query } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver()
+@Resolver(of => Restaurant) // 우리의 resolver는 Restaurant의 resolver가 됐다. 굳이 이렇게 쓰지 않아도 된다.
 export class RestaurantsResolver {
   // Query는 typeFunc를 받는다. 무슨 의미이냐면,
   // Query가 return하고자 하는 type을 return하는 function이여야 한다는것이다.
   // Query는 첫번째 args로 함수가 필요하다.
-  @Query(returns => Boolean)
-  isPizzaGood(): Boolean {
+
+  @Query(returns => Restaurant)
+  myRestaurant() {
     return true;
   }
 }
