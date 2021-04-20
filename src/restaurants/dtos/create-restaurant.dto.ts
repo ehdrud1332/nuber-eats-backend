2ad2abc6a1c4075b94cc3ebbbe534313a5f2a7a1
castle.jaemin <ehdrud1332@gmail.com>
2021-04-20 22:38:43 +0900

    3.6 Optional Types and Columns

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,8 +1,6 @@
 // dto는  data transfer object
 
-import { Field, InputType, OmitType } from '@nestjs/graphql';
-import { IsBoolean, IsString, Length } from 'class-validator';
-import { Column } from 'typeorm';
+import { InputType, OmitType } from '@nestjs/graphql';
 import { Restaurant } from '../entities/restaurant.entity';
 // inputType은 내가 input 하고자 하는 data의 type이다.
 // ArgsType은 기본적으로 이것들을 분리된 args로써 정의할 수 있게 해준다.
@@ -10,8 +8,4 @@ import { Restaurant } from '../entities/restaurant.entity';
 // InputType은 하나의 object이다 args로써 graphQL에 전달하기 위한 용도.
 // inputType은 기본적으로 object 통째로 전달할 수 있도록 해준다.
 @InputType()
-export class CreateRestaurantDto extends OmitType(
-  Restaurant,
-  ['id'],
-  InputType,
-) {}
+export class CreateRestaurantDto extends OmitType(Restaurant, ['id']) {}
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,6 +1,7 @@
 // entity는 데이터베이스에 저장되는 데이터의 형태를 보여주는 모델이다.
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
 
 // Repository는 Entity랑 상호작용하는걸 담당한다.
 // NestJS에서 Data Mapper 방법을 쓰는 이유는 NestJs + TypeORM 개발 환경에서 Reposioty를 사용하는 모듈을 쓸 수 있기 때문이다.
@@ -12,30 +13,40 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 // 이렇게 하면 클래스 하나로 graphQL 스키마와 DB에 저장되는 실제 데이터의 형식을 만들 수 있다.
 // @InputType({ isAbstract: true })
 // ⇧ 이게 무슨 말이냐면 inputType이 스키마에 포함되지 않길 원한다는 것
+@InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
 export class Restaurant {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   @Field(type => Number)
   id: number;
 
   @Field(type => String) // 이 부분은 graphQl을 위한 return
   @Column()
+  @IsString()
+  @Length(5)
   name: string; // 이건 ts를 위한 return
 
-  @Field(type => Boolean, { nullable: true })
-  @Column()
-  isVegan?: boolean;
+  @Field(type => Boolean, { defaultValue: true }) // GraphQL을 위한 코드
+  @Column({ default: true }) // DB를 위한 코드
+  // IsOptional은  value가 누락됐는지 확인하고, 없다면 모든 validator를 무시한다.
+  // IsOptional은 해당 필드를 보내거나 보내지 않을 수 없다는 걸 의미한다.
+  @IsOptional() // DTO를 위한 코드
+  @IsBoolean() // DTO를 위한 코드
+  isVegan: boolean;
 
   @Field(type => String)
   @Column()
+  @IsString()
   address: string;
 
-  @Field(type => String)
-  @Column()
-  ownerName: string;
-
-  @Field(type => String)
-  @Column()
-  categoryName: string;
+  // @Field(type => String)
+  // @Column()
+  // @IsString()
+  // ownerName: string;
+  //
+  // @Field(type => String)
+  // @Column()
+  // @IsString()
+  // categoryName: string;
 }
