7284f4319717bd2df8de57114baa6e09744a8268
castle.jaemin <ehdrud1332@gmail.com>
2021-04-20 19:49:58 +0900

    3.5 Mapped Types
    Dto, entity를 따로 코드를 작성하지 않고 같이 업데이트 가능하게 코드를 짜보자

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,29 +1,17 @@
 // dto는  data transfer object
 
-import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { Field, InputType, OmitType } from '@nestjs/graphql';
 import { IsBoolean, IsString, Length } from 'class-validator';
 import { Column } from 'typeorm';
+import { Restaurant } from '../entities/restaurant.entity';
 // inputType은 내가 input 하고자 하는 data의 type이다.
 // ArgsType은 기본적으로 이것들을 분리된 args로써 정의할 수 있게 해준다.
 
 // InputType은 하나의 object이다 args로써 graphQL에 전달하기 위한 용도.
 // inputType은 기본적으로 object 통째로 전달할 수 있도록 해준다.
-@ArgsType()
-export class CreateRestaurantDto {
-  @Field(type => String)
-  @IsString()
-  @Length(5, 10)
-  name: string;
-
-  @Field(type => Boolean)
-  @IsBoolean()
-  isVegan: boolean;
-
-  @Field(type => String)
-  @IsString()
-  address: string;
-
-  @Field(type => String)
-  @IsString()
-  ownerName: string;
-}
+@InputType()
+export class CreateRestaurantDto extends OmitType(
+  Restaurant,
+  ['id'],
+  InputType,
+) {}
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,5 +1,5 @@
 // entity는 데이터베이스에 저장되는 데이터의 형태를 보여주는 모델이다.
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 // Repository는 Entity랑 상호작용하는걸 담당한다.
@@ -10,6 +10,8 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 // objectType은 자동으로 스키마를 빌드하기 위해 사용하는 GraphQL decorator
 // Entity는 TypeORM이 DB에 이걸 저장하게 해준다.
 // 이렇게 하면 클래스 하나로 graphQL 스키마와 DB에 저장되는 실제 데이터의 형식을 만들 수 있다.
+// @InputType({ isAbstract: true })
+// ⇧ 이게 무슨 말이냐면 inputType이 스키마에 포함되지 않길 원한다는 것
 @ObjectType()
 @Entity()
 export class Restaurant {
